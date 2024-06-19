6fc8c70fa3477372668f820a083f861e2d79c1fa
Shubham15Bit <shubham@beimagine.tech>
2024-06-19 21:58:09 +0530

    frontend implementation using shadcn

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,25 +1,56 @@
 "use client";
-import { useSession, signIn, signOut } from "next-auth/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useDebounceValue } from "usehooks-ts";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+import { signUpSchema } from "@/schemas/signUpSchema";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
 
-export default function Component() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button
-        className="bg-orange-500 px-3 py-1 m-4 rounded"
-        onClick={() => signIn()}
-      >
-        Sign in
-      </button>
-    </>
-  );
-}
+const page = () => {
+  const [username, setUsername] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [iseCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, seIsSubmitting] = useState(false);
+  const debouncedUsername = useDebounceValue(username, 300);
+  const { toast } = useToast();
+  const router = useRouter();
+
+  //zod implementation
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+  useEffect(() => {
+    const checkUsernameUnique = async () => {
+      if (debouncedUsername) {
+        setIsCheckingUsername(true);
+        setUsernameMessage("");
+        try {
+          const response = await axios.get(
+            `/api/check-username-unique?username=${debouncedUsername}`
+          );
+          setUsernameMessage(response.data.message);
+        } catch (error) {
+          const axiosError = error as AxiosError<ApiResponse>;
+          setUsernameMessage(
+            axiosError.response?.data.message ?? "Error checking username"
+          );
+        } finally {
+          setIsCheckingUsername(false);
+        }
+      }
+    };
+  }, [debouncedUsername]);
+  return <div>page</div>;
+};
+
+export default page;
--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -7,7 +7,7 @@ export const usernameValidation = z
   .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special character");
 
 export const signUpSchema = z.object({
-  usernameValidation: usernameValidation,
+  username: usernameValidation,
   email: z.string().email({ message: "Invalid email address" }),
   password: z
     .string()
